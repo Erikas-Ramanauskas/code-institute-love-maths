614a12b074a2b543a8513fa28775c0282d0f5ce7
Erikas Ramanauskas <ramanauskas.erikas@gmail.com>
2023-03-12 15:07:12 +0000

    Completed division chalange

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -123,7 +123,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand1').textContent = operand1 / operand2 % 1 === 0 ? operand1 : operand1 * operand2;
     document.getElementById(`operand2`).textContent = operand2;
     document.getElementById(`operator`).textContent = "/";
 }
