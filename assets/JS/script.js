284e20f38b12f1ad0d1de70b0a80d512d1a05f0b
Erikas Ramanauskas <ramanauskas.erikas@gmail.com>
2023-03-12 14:57:51 +0000

    Add favication icon and tidy up the user

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -14,6 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         })
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
     runGame("addition")
 })
 
@@ -23,6 +29,10 @@ document.addEventListener("DOMContentLoaded", function () {
  */
 
 function runGame(gameType) {
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
